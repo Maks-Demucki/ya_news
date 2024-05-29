8377c54bda7ee8e2169beafc93718656c7d29fe2
Maks Demucki <ya.maksim777d@yandex.ru>
2024-05-29 20:47:16 +0600

    Проверка доступа на главную страницу и регистрацию, вход и выход для анонимного пользователя

---
+++ news/tests/test_routes.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from news.models import News
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                responce = self.client.get(url)
+                self.assertEqual(responce.status_code, HTTPStatus.OK)
+

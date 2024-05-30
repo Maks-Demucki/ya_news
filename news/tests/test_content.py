cce08900388c82aa6eaadda591ce6f38e87a8c03
Maks Demucki <ya.maksim777d@yandex.ru>
2024-05-30 21:20:49 +0600

    Тест сортировки новостей по дате публикации

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
@@ -10,8 +12,13 @@ class TestContent(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        today = datetime.today()
         all_news = [
-            News(title=f'Новость {index}', text='Просто текст.')
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         ]
         News.objects.bulk_create(all_news)
@@ -21,3 +28,10 @@ class TestContent(TestCase):
         object_list = responce.context['object_list']
         news_count = object_list.count()
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        responce = self.client.get(self.HOME_URL)
+        object_list = responce.context['object_list']
+        all_dates = [news.date for news in object_list]
+        sorted_dates = sorted(all_dates, reverse=True)
+        self.assertEqual(all_dates, sorted_dates)
